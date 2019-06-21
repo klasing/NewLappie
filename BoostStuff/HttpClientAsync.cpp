018497c82655f7b81c4006486c327f3940665c84
klasing1159 <klasing1159@gmail.com>
2019-06-21 17:49:48 +0200

    21-06-2019 17:49

--- BoostStuff/HttpClientAsync.cpp
+++ BoostStuff/HttpClientAsync.cpp
@@ -1,5 +1,6 @@
 // HttpClientAsync.cpp
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 // File client
@@ -9,10 +10,24 @@ using namespace std;
 //****************************************************************************
 //*                     get_input_from_user
 //****************************************************************************
-string get_input_from_user(const string& hdr, const string& val) {
+string get_input_from_user(
+	const string& hdr,
+	const int& width_header,
+	const string& val,
+	const int& width_value)
+{
 	string input_from_user = "";
 
-	cout << hdr << val << " ";
+	cout << setw(width_header)
+		<< setiosflags(ios_base::left)
+		<< setfill('.')
+		<< hdr
+		<< setw(width_value)
+		<< setiosflags(ios_base::right)
+		<< setfill('.')
+		<< val
+		<< ": ";
+	cout.unsetf(ios::adjustfield);
 	getline(cin, input_from_user, '\n');
 
 	return input_from_user.length() > 0 ? input_from_user : val;
@@ -22,47 +37,119 @@ string get_input_from_user(const string& hdr, const string& val) {
 //*                     get_connect_params_from_user
 //****************************************************************************
 void get_connect_params_from_user(
-	const string& IP_ADDRESS, string& ip_address,
-	const string& PORT_NUMBER, string& port_number) {
+	string& ip_address,
+	string& port_number)
+{
+	const string HDR_IP_ADDRESS = "IP Address";
+	const string HDR_PORT_NUMBER = "Port";
 
-	const string HDR_IP_ADDRESS = "IP Address...: ";
-	const string HDR_PORT_NUMBER = "Port.........: ";
+	int width_header = HDR_IP_ADDRESS.length() + 3;
+	int width_value = ip_address.length();
 
-	ip_address = get_input_from_user(HDR_IP_ADDRESS, IP_ADDRESS);
-	port_number = get_input_from_user(HDR_PORT_NUMBER, PORT_NUMBER);
+	ip_address = get_input_from_user(
+		HDR_IP_ADDRESS,
+		width_header,
+		ip_address,
+		width_value
+	);
+	port_number = get_input_from_user(
+		HDR_PORT_NUMBER,
+		width_header,
+		port_number,
+		width_value
+	);
 }
 
 //****************************************************************************
 //*                     get_file_names_from_user
 //****************************************************************************
 void get_file_names_from_user(
-	const string& FILE_NAME_ON_SOURCE,
-	string& file_name_on_source,
-	const string& FILE_NAME_ON_TARGET,
-	string& file_name_on_target) {
+	const string& transfer_mode,
+	string& file_name_on_target,
+	string& file_name_on_destination)
+{
+	string HDR_FILE_NAME_ON_TARGET = "";
+	string HDR_FILE_NAME_ON_DESTINATION = "";
 
-	const string HDR_FILE_NAME_ON_SOURCE = "Source...: ";
-	const string HDR_FILE_NAME_ON_TARGET = "Target...: ";
+	if (transfer_mode == "download") {
+		HDR_FILE_NAME_ON_TARGET = "Target (server)";
+		HDR_FILE_NAME_ON_DESTINATION = "Destination (client)";
+	}
+	else {
+		HDR_FILE_NAME_ON_TARGET = "Target (client)";
+		HDR_FILE_NAME_ON_DESTINATION = "Destination (server)";
+	}
 
-	file_name_on_source = get_input_from_user(
-		HDR_FILE_NAME_ON_SOURCE, FILE_NAME_ON_SOURCE);
+	int width_header = HDR_FILE_NAME_ON_DESTINATION.length() + 3;
+
+	int width_value =
+		(file_name_on_target.length() > file_name_on_destination.length()) ?
+		file_name_on_target.length() :
+		file_name_on_destination.length();
+
 	file_name_on_target = get_input_from_user(
-		HDR_FILE_NAME_ON_TARGET, FILE_NAME_ON_TARGET);
+		HDR_FILE_NAME_ON_TARGET,
+		width_header,
+		file_name_on_target,
+		width_value
+	);
+
+	file_name_on_destination = get_input_from_user(
+		HDR_FILE_NAME_ON_DESTINATION,
+		width_header,
+		file_name_on_destination,
+		width_value
+	);
 }
 
 //****************************************************************************
-//*                     main
+//*                     do_transfer
+//****************************************************************************
+void do_transfer(
+	const string& transfer_mode,
+	const string& file_name_on_target,
+	const string& file_name_on_destination,
+	const string& ip_address,
+	const string& port_number)
+{
+	char cmd[] = "http-client-async";
+	char* mode = const_cast<char*>(transfer_mode.c_str());
+	char* file_target = const_cast<char*>(file_name_on_target.c_str());
+	char* file_destination = const_cast<char*>(file_name_on_destination.c_str());
+	char* host = const_cast<char*>(ip_address.c_str());
+	char* port = const_cast<char*>(port_number.c_str());
+	char target[] = "/";
+	char version[] = "1.0";
+
+	int argc = 8;
+	char* argv[] = { cmd, mode, file_target, file_destination,
+		host, port, target, version };
+	ns_http_client_async::http_client_async(argc, argv);
+}
+
+//****************************************************************************
+//                      main
 //****************************************************************************
 int main() {
 	const string IP_ADDRESS = "192.168.178.14";
 	const string PORT_NUMBER = "8080";
-	const string FILE_NAME_ON_SERVER = "index.html";
-	const string FILE_NAME_ON_CLIENT = "index_client.html";
 
 	string ip_address = IP_ADDRESS;
 	string port_number = PORT_NUMBER;
-	string file_name_on_server = "";
-	string file_name_on_client = "";
+
+	string transfer_mode = "download";
+
+	const string FILE_NAME_ON_SERVER_AT_DOWNLOAD = "index.html";
+	const string FILE_NAME_ON_CLIENT_AT_DOWNLOAD = "index_client.html";
+
+	string file_name_on_server_at_download = FILE_NAME_ON_SERVER_AT_DOWNLOAD;
+	string file_name_on_client_at_download = FILE_NAME_ON_CLIENT_AT_DOWNLOAD;
+
+	const string FILE_NAME_ON_CLIENT_AT_UPLOAD = "index_client.html";
+	const string FILE_NAME_ON_SERVER_AT_UPLOAD = "index_server.html";
+
+	string file_name_on_client_at_upload = FILE_NAME_ON_CLIENT_AT_UPLOAD;
+	string file_name_on_server_at_upload = FILE_NAME_ON_SERVER_AT_UPLOAD;
 
 	bool bProceed = true;
 	int iChar;
@@ -81,34 +168,45 @@ int main() {
 
 		switch (iChar) {
 		case 1:
-			get_connect_params_from_user(IP_ADDRESS, ip_address,
-				PORT_NUMBER, port_number);
-			cout << ip_address << ", " << port_number << endl;
+			get_connect_params_from_user(
+				ip_address,
+				port_number
+			);
+			cerr << "IP Address: " << ip_address << ", Port: " << port_number << endl;
 			break;
-		case 2: {
-			get_file_names_from_user(FILE_NAME_ON_SERVER,
-				file_name_on_server,
-				FILE_NAME_ON_CLIENT,
-				file_name_on_client);
-
-			file_name_on_server.insert(0, "/");
-
-			// do download
-			char cmd[] = "http-client-async";
-			char* host = const_cast<char*>(ip_address.c_str());
-			char* port = const_cast<char*>(port_number.c_str());
-			char* target = const_cast<char*>(file_name_on_server.c_str());
-			char* destination = const_cast<char*>(file_name_on_client.c_str());
-			char version[] = "1.0";
-			char* argv[] = { cmd, host , port, target, destination, version };
-			ns_http_client_async::http_client_async(6, argv);
-		}
-				break;
+		case 2:
+			transfer_mode = "download";
+			get_file_names_from_user(
+				transfer_mode,
+				file_name_on_server_at_download,
+				file_name_on_client_at_download
+			);
+			cerr << "Target (server): " << file_name_on_server_at_download << endl;
+			cerr << "Destination (client): " << file_name_on_client_at_download << endl;
+			do_transfer(
+				transfer_mode,
+				file_name_on_server_at_download,
+				file_name_on_client_at_download,
+				ip_address,
+				port_number
+			);
+			break;
 		case 3:
-			get_file_names_from_user(FILE_NAME_ON_CLIENT,
-				file_name_on_client,
-				FILE_NAME_ON_SERVER,
-				file_name_on_server);
+			transfer_mode = "upload";
+			get_file_names_from_user(
+				transfer_mode,
+				file_name_on_client_at_upload,
+				file_name_on_server_at_upload
+			);
+			cerr << "Target (client): " << file_name_on_client_at_upload << endl;
+			cerr << "Destination (server): " << file_name_on_server_at_upload << endl;
+			do_transfer(
+				transfer_mode,
+				file_name_on_client_at_upload,
+				file_name_on_server_at_upload,
+				ip_address,
+				port_number
+			);
 			break;
 		case 0:
 			// the user wants to terminate
@@ -117,6 +215,7 @@ int main() {
 		default:
 			// the input, given by the user, is not an available option
 			cout << "The entered number is not recognized, please try again." << endl;
+			break;
 		} // eof switch
 	}
 
--- BoostStuff/http_client_async.cpp
+++ BoostStuff/http_client_async.cpp
@@ -7,65 +7,92 @@
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
 #include <boost/asio/strand.hpp>
-
-#include <boost/filesystem/fstream.hpp> // ADDED
-
+#include <boost/filesystem/fstream.hpp>
 #include <cstdlib>
 #include <functional>
 #include <iostream>
 #include <memory>
 #include <string>
 
-namespace beast = boost::beast;
-namespace http = beast::http;
-namespace net = boost::asio;
-
-using tcp = boost::asio::ip::tcp;
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 namespace ns_http_client_async {
-	//---------------------------------------------------------------------------
 	//***************************************************************************
-	//                     fail
+	//*                    fail
 	//***************************************************************************
-	// Report failure
-	inline void fail(beast::error_code ec, char const* what) {
+	// Report a failure
+	inline void
+		fail(beast::error_code ec, char const* what)
+	{
 		std::cerr << what << ": " << ec.message() << "\n";
 	}
 
 	//***************************************************************************
-	//                     session
+	//*                    session
 	//***************************************************************************
 	// Performs an HTTP GET and prints the response
-	class session : public std::enable_shared_from_this<session> {
+	class session : public std::enable_shared_from_this<session>
+	{
 		tcp::resolver resolver_;
 		beast::tcp_stream stream_;
 		beast::flat_buffer buffer_; // (Must persist between reads)
 		http::request<http::empty_body> req_;
 		http::response<http::string_body> res_;
-
-		std::string destination_ = "";
+		std::string mode_ = "";
+		std::string file_target_ = "";
+		std::string file_destination_ = "";
 
 	public:
 		// Objects are constructed with a strand to
 		// ensure that handlers do not execute concurrently.
-		explicit session(net::io_context& ioc)
+		explicit
+			session(net::io_context& ioc)
 			: resolver_(net::make_strand(ioc))
-			, stream_(net::make_strand(ioc)) {}
+			, stream_(net::make_strand(ioc))
+		{
+		}
 
 		// Start the asynchronous operation
-		void run(
-			char const* host,
-			char const* port,
-			char const* target,
-			int version,
-			char const* destination) {
-			// Set up an HTTP GET request message
-			req_.version(version);
-			req_.method(http::verb::get);
-			req_.target(target);
-			req_.set(http::field::host, host);
-			req_.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+		void
+			run(
+				char* mode,
+				char* file_target,
+				char* file_destination,
+				char* host,
+				char* port,
+				char* target,
+				int version)
+		{
+			mode_ = mode;
 
-			destination_ = destination;
+			if (!std::strcmp("download", mode)) {
+				file_target_ = "/" + std::string(file_target);
+				file_destination_ = file_destination;
+				// Set up an HTTP GET request message
+				req_.method(http::verb::get);
+				req_.target(file_target_);
+				req_.version(version);
+				req_.set(http::field::host, host);
+				req_.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+			}
+
+			if (!std::strcmp("upload", mode)) {
+				file_target_ = file_target;
+				file_destination_ = "/" + std::string(file_destination);
+				// Set up an HTTP PUT request message
+				req_.method(http::verb::put);
+				req_.target(file_destination_);
+				req_.version(version);
+				req_.set(http::field::host, host);
+				req_.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+				// experimental
+				req_.set(http::field::content_type, "text/plain");
+				std::string str = "bla bla";
+				req_.set(http::field::body, str);
+				req_.prepare_payload();
+			}
 
 			// Look up the domain name
 			resolver_.async_resolve(
@@ -76,9 +103,11 @@ namespace ns_http_client_async {
 					shared_from_this()));
 		}
 
-		void on_resolve(
-			beast::error_code ec,
-			tcp::resolver::results_type results) {
+		void
+			on_resolve(
+				beast::error_code ec,
+				tcp::resolver::results_type results)
+		{
 			if (ec)
 				return fail(ec, "resolve");
 
@@ -93,7 +122,9 @@ namespace ns_http_client_async {
 					shared_from_this()));
 		}
 
-		void on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type) {
+		void
+			on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type)
+		{
 			if (ec)
 				return fail(ec, "connect");
 
@@ -107,8 +138,11 @@ namespace ns_http_client_async {
 					shared_from_this()));
 		}
 
-		void on_write(beast::error_code ec,
-			std::size_t bytes_transferred) {
+		void
+			on_write(
+				beast::error_code ec,
+				std::size_t bytes_transferred)
+		{
 			boost::ignore_unused(bytes_transferred);
 
 			if (ec)
@@ -121,9 +155,11 @@ namespace ns_http_client_async {
 					shared_from_this()));
 		}
 
-		void on_read(
-			beast::error_code ec,
-			std::size_t bytes_transferred) {
+		void
+			on_read(
+				beast::error_code ec,
+				std::size_t bytes_transferred)
+		{
 			boost::ignore_unused(bytes_transferred);
 
 			if (ec)
@@ -132,16 +168,18 @@ namespace ns_http_client_async {
 			// Write the message to standard out
 			std::cout << res_ << std::endl;
 
-			// ADDED Write the message to file stream
-			boost::filesystem::path p{ destination_ };
-			boost::filesystem::ofstream ofs{ p };
-			ofs << res_.body();
+			// Store the received file on disk
+			if (!std::strcmp("download", mode_.c_str())) {
+				boost::filesystem::path p{ file_destination_ };
+				boost::filesystem::ofstream ofs{ p };
+				ofs << res_.body();
+			}
 
 			// Gracefully close the socket
 			stream_.socket().shutdown(tcp::socket::shutdown_both, ec);
 
 			// not_connected happens sometimes so don't bother reporting it.
-			if (ec && ec != beast::errc::not_a_directory)
+			if (ec && ec != beast::errc::not_connected)
 				return fail(ec, "shutdown");
 
 			// If we get here then the connection is closed gracefully
@@ -149,31 +187,42 @@ namespace ns_http_client_async {
 	};
 
 	//***************************************************************************
-	//                     http_client_async
+	//*                    http_client_async
 	//***************************************************************************
-	inline int http_client_async(int argc, char** argv) {
-		// Check command lone arguments
-		if (argc != 5 && argc != 6) {
-			std::cerr <<
-				"Usage: http-client-async <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
+	inline int http_client_async(int argc, char** argv)
+	{
+		// Check command line arguments.
+		if (argc != 7 && argc != 8)
+		{
+			std::cout <<
+				"Usage: http-client-async <mode: download or upload> <target file> <destination file> <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
 				"Example:\n" <<
-				"    http-client-async www.example.com 80 / /\n" <<
-				"    http-client-async www.example.com 80 / / 1.0";
+				"    http-client-async download fileT fileD 127.0.0.1 8080 / \n" <<
+				"    http-client-async upload fileT fileD 127.0.0.1 8080 / 1.0\n";
 			return EXIT_FAILURE;
 		}
-		auto const host = argv[1];
-		auto const port = argv[2];
-		auto const target = argv[3];
-		auto const destination = argv[4];
-		int version = argc == 6 && !std::strcmp("1.0", argv[5]) ? 10 : 11;
+		auto const mode = argv[1];
+		auto const file_target = argv[2];
+		auto const file_destination = argv[3];
+		auto const host = argv[4];
+		auto const port = argv[5];
+		auto const target = argv[6];
+		int version = argc == 8 && !std::strcmp("1.0", argv[7]) ? 10 : 11;
 
 		// The io_context is required for all I/O
 		net::io_context ioc;
 
 		// Launch the asynchronous operation
 		// set the file name where a downloaded file is stored on the client side
-		std::make_shared<session>(ioc)->run(host, port, target, version,
-			destination);
+		// set parameter to determine a download or an upload
+		std::make_shared<session>(ioc)->run(
+			mode,
+			file_target,
+			file_destination,
+			host,
+			port,
+			target,
+			version);
 
 		// Run the I/O service. The call will return when
 		// the get operation is complete.
